93a0b564d8b099a06747fbd9fe51245087228e4a
Samuel <samuferfort@gmail.com>
2024-09-04 15:24:23 +0200

    add group filter

--- src/utils/filterItems.js
+++ src/utils/filterItems.js
@@ -1,6 +1,6 @@
 const filterItems = (items, filterText, activeTab) => {
   return items.filter((item) => {
-    if (activeTab === "chats") {
+    if (activeTab === "chats" || activeTab === "groups") {
       return item.name.toLowerCase().includes(filterText.toLowerCase());
     } else {
       return (item.firstName + " " + item.lastName)
