422ecea5bd6042d95fe45bf7f8944363d1ed4606
Samuel <samuferfort@gmail.com>
2024-09-06 22:21:50 +0200

    change from universal time to local time

--- src/utils/formatMessageTime.js
+++ src/utils/formatMessageTime.js
@@ -1,10 +1,8 @@
 export default function formatTimestampToHHMM(timestamp) {
-    const date = new Date(timestamp);
-  
-    const hours = date.getUTCHours().toString().padStart(2, '0');
-    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-  
-    return `${hours}:${minutes}`;
-  }
+  const date = new Date(timestamp);
 
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
 
+  return `${hours}:${minutes}`;
+}
